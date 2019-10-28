46f7dea0377d1da60a3eb8d0bd31cf8fdbe5dfa4
jollyjerr <jollyjerr@gmail.com>
2019-10-27 19:11:02 -0600

    remove some boiler

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,11 @@
 <template>
   <div id="app">
     <nav id="nav">
+      <img src="./assets/logo.jpeg" alt="hello world">
       <router-link to="/">Home</router-link> 
-      <router-link to="/projects">Projects</router-link>
     </nav>
     <router-view/>
-    <footer>
+    <footer id="footer">
       <h3>Footer</h3>
     </footer>
   </div>
@@ -13,20 +13,42 @@
 
 <style lang="scss">
 @import url('https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap');
+@import './assets/variables.scss';
+body {
+  padding: 0;
+  margin: 0;
+  width: 100%;
+  height: 100%;
+}
 #app {
   font-family:'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
+  color: $text;
+  background-color: $background;
+  width: 100%;
+  height: 100%;
 }
 #nav {
   padding: 30px;
   a {
     font-weight: bold;
-    color: #2c3e50;
+    color: $title;
+    padding: 0 $normal;
+    position: absolute;
+    right: 0;
     &.router-link-exact-active {
-      color: #42b983;
+      color: $important;
     }
   }
+  img {
+    width: 5rem;
+    border-radius: 100%;
+  }
+}
+#footer {
+  width:100%;
+  height:60px;   /* Height of the footer */
+  background-color: $dull;
 }
 </style>
