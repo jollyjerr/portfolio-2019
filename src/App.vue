703005d7af0185b7dc40ff7b271ff013801bc0ae
jollyjerr <jollyjerr@gmail.com>
2019-10-27 18:23:57 -0600

    establish higharchy

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav id="nav">
+      <router-link to="/">Home</router-link> 
+      <router-link to="/projects">Projects</router-link>
+    </nav>
     <router-view/>
+    <footer>
+      <h3>Footer</h3>
+    </footer>
   </div>
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css?family=Montserrat:400,600&display=swap');
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family:'Montserrat', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
